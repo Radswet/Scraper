6e545a234d737f77f31a897d717c815d0ca14f36
Radswet <fvergaralobos@gmail.com>
2022-03-03 01:56:41 -0300

    Add Clandent

--- Stores/Clandent.py
+++ Stores/Clandent.py
@@ -9,29 +9,29 @@ mydb = client["Cluster0"]
 mycol = mydb["Clandent"]
 
 
-#website = 'https://www.clandent.cl/product-category/radiologia/?v=cda73baae416'
-#
-#result = requests.get(website)
-#soup = BeautifulSoup(result.text, 'html.parser')
-#
-#Categories = soup.findAll('li', class_='cat-item')
-#
-#Categories_urls = []
-#
-#for c in Categories:
-#    if not 'universidades' in c.find('a')['href']:
-#        Categories_urls.append(c.find('a')['href'])
-#
-#Products_urls = []
-#
-#for url in Categories_urls:
-#    result = requests.get(url)
-#    soup = BeautifulSoup(result.text, 'html.parser')
-#    Product = soup.findAll('div', class_='product-wrapper')
-#
-#    for p in Product:
-#        Products_urls.append(p.find('a')['href'])
-#    print(Products_urls)
+website = 'https://www.clandent.cl/product-category/radiologia/?v=cda73baae416'
+
+result = requests.get(website)
+soup = BeautifulSoup(result.text, 'html.parser')
+
+Categories = soup.findAll('li', class_='cat-item')
+
+Categories_urls = []
+
+for c in Categories:
+    if not 'universidades' in c.find('a')['href']:
+        Categories_urls.append(c.find('a')['href'])
+
+Products_urls = []
+
+for url in Categories_urls:
+    result = requests.get(url)
+    soup = BeautifulSoup(result.text, 'html.parser')
+    Product = soup.findAll('div', class_='product-wrapper')
+
+    for p in Product:
+        Products_urls.append(p.find('a')['href'])
+    print(Products_urls)
 Products_urls = ["https://www.clandent.cl/producto/adhesivo-para-cubeta-kerr/?v=cda73baae416"]
 
 for url in Products_urls:
@@ -45,7 +45,7 @@ for url in Products_urls:
         price = soup.find('bdi').text
         sku = soup.find('span', {'class': 'sku'}).text
 
-        Product = {'brand':brand,'name':name,'price':price,'sku':sku}
+        Product = {'Brand':brand,'Name':name,'Price':price,'SKU':sku,'URL':url}
         print(Product)
         mycol.insert_one(Product)
     except:
