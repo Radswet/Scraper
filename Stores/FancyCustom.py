3b5265eaf7c1213af137707da1babe37213a7db7
Radswet <fvergaralobos@gmail.com>
2022-03-03 23:21:01 -0300

    add Fancy Custom

--- Stores/FancyCustom.py
+++ Stores/FancyCustom.py
@@ -1,33 +1,68 @@
+from math import prod
 from bs4 import BeautifulSoup
 import requests
 import pymongo
-from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
+def categories():
+    return [
+        "artisans",
+        "deskmats-1",
+        "keycaps",
+        "switches",
+        "placas",
+        "pcb",
+        "carcasas",
+        "accesorios",
+        "cables",
+        "servicios",
+        "gb-keycaps",
+        "teclados",
+        "gb-switches",
+        "extras",
+        "kits",
+    ]
+
+
 CONNECTION_STRING = os.getenv('CONNECTION_STRING')
 
 client = pymongo.MongoClient(CONNECTION_STRING)
+
 mydb = client["Cluster0"]
-mycol = mydb["CyFstore"]
+mycol = mydb["FancyCustom"]
 
 
 product_urls = []
 
-website = 'https://fancycustoms.com/collections/keycaps'
+website = 'https://fancycustoms.com/collections/{}'
+categories = categories()
 
-result = requests.get(website)
-content = result.text
-soup = BeautifulSoup(content, 'html.parser')
 
-while True:
-        products = soup.find('div', class_='tt-description')
+for category in categories:
+        url = website.format(category)      
+        result = requests.get(url)
+        soup = BeautifulSoup(result.text, 'html.parser')
 
-        if not products:
-                break
+        product = soup.findAll('h2',class_="tt-title prod-thumb-title-color")
+        
+        for p in product:
+                product_urls.append(p.find('a')['href'].replace("/collections/", ""))
+                #print(p.find('a')['href'].replace("/collections/", ""))
 
-        for product in products:
-                product_urls.append(product['href'])
+for urls in product_urls:
+        url = website.format(urls)    
+        result = requests.get(url)
+        soup = BeautifulSoup(result.text, 'html.parser')
 
-        print(product_urls)
+        name = soup.find('h1',class_="tt-title").text
+        try:
+                price = soup.find('span',class_="new-price").text
+                data = {"name":name,"price":price}
+        except:
+                sale = soup.find('span',class_="sale-price").text
+                old = soup.find('span',class_="old-price").text
+                data = {"name":name,"sale-price":sale,"old-price":old}
+
+        mycol.insert_one(data)
